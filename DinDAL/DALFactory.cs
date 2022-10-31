1395f8944fd7863124c46b3ffc8a8c12bd9a13a9
Joanes Gonzaga <joanesgonzaga.dev@gmail.com>
2022-10-31 19:57:37 -0300

    A DAL caller class

---
+++ DinDAL/DALFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinDAL
+{
+    public static class DALFactory
+    {
+        public static T CreateDAL<T>() where T : DAL, new()
+        {
+            return new T();
+        }
+    }
+}
